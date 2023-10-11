a05052bb1dd056e6e4d2a86c8646405ab5e3f78b
CarmineD8 <carmine.recchiuto@dibris.unige.it>
2023-10-11 10:16:25 +0000

    first commit

---
+++ src/opencv_ros2_subscriber.cpp
@@ -0,0 +1,47 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include <image_transport/image_transport.hpp>
+#include <memory>
+#include "cv_bridge/cv_bridge.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+static const std::string OPENCV_WINDOW = "Image window";
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+  public:
+    MinimalSubscriber()
+    : Node("minimal_subscriber")
+    {
+      subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      cv::namedWindow(OPENCV_WINDOW);
+    }
+    
+
+  private:
+    void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
+    {     
+   cv_bridge::CvImagePtr cv_ptr;
+
+      cv_ptr = cv_bridge::toCvCopy(msg,msg->encoding);
+     if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+     cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+     cv::waitKey(3);
+    }
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
